3d2869676246173767ebc12a505c8fcba4349de7
Isaac Assegai <isaac.a.travers@gmail.com>
2014-08-20 20:03:11 -0700

    Fixed Typo

--- chicoer.js
+++ chicoer.js
@@ -1,6 +1,6 @@
 javascript:(
 	function(){
-		$("#gregbox-outer").hide();
+		$("#gregbox-outer").hide();
 		$("#gregbox-signInTab").hide();
 		$('#gregbox-closeBtn').css('display', 'none');
 		$('body > div:nth-child(20)').css('display', 'none');
