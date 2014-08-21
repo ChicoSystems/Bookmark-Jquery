16de1ef58af667fc26a70d1c66111613a09a4330
Isaac Assegai <isaac.a.travers@gmail.com>
2014-08-20 19:42:45 -0700

    added the chicoer.js file

---
+++ chicoer.js
@@ -0,0 +1,7 @@
+javascript:(
+	function(){
+		$("#gregbox-outer").hide();
+		$("#gregbox-signInTab").hide();
+		$('#gregbox-closeBtn').css('display', 'none');
+		$('body > div:nth-child(20)').css('display', 'none');
+	}());
