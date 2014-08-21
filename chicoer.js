3ed285845d5b179fb15d6e3a46dc06a2fd408b92
Isaac Assegai <isaac.a.travers@gmail.com>
2014-08-20 20:04:25 -0700

    added notes

--- chicoer.js
+++ chicoer.js
@@ -1,5 +1,11 @@
 javascript:(
 	function(){
+		/* Author: Isaac Assegai
++		 * ChicoER only allow you to view an article or two a day.
++		 * If this script is copied into a bookmark. It will search
++		 * for the elements that block your view and tell you to
++ 		 * subscribe, and hide those elements.
++		 */
 		$("#gregbox-outer").hide();
 		$("#gregbox-signInTab").hide();
 		$('#gregbox-closeBtn').css('display', 'none');
